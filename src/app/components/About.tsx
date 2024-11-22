6fb9c5835a6261bcd3b04fe8d1faa6391858fb8a
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-11-22 20:28:19 +0200

    feat(about): finished about section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,18 +6,51 @@ import React from 'react';
 
 export default function About() {
 	return (
-		<div className='bg-off-white px-5 py-24'>
+		<div id='about' className='bg-off-white px-5 py-16 laptop:py-24'>
 			<motion.div {...motionStyles} className='max-width'>
-				<div className='flex items-center justify-center'>
-					<div>
+				<div className='flex flex-col laptop:flex-row items-start justify-center'>
+					<div className='mb-10 laptop:mb-0'>
 						<Image
 							src={'/crunch-woman.jpg'}
-							width={500}
-							height={450}
+							width={370}
+							height={400}
 							alt='crunch woman'
+							className='rounded-3xl w-[476px]'
 						/>
 					</div>
-					<div>Philisophy</div>
+					<div className='laptop:ml-12'>
+						<h2 className='text-5xl font-bold'>Philosophy</h2>
+						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-2xl'>
+							We identify a niche in the market for premium fitness attire and
+							outdoor equipment at economic rates. Our vision is to span this
+							void by establishing a retail framework that puts these items
+							within reach of a wider audience.
+						</p>
+						<button className='mt-5'>Read more</button>
+					</div>
+				</div>
+			</motion.div>
+			<motion.div {...motionStyles} className='max-width'>
+				<div className='flex flex-col laptop:flex-row items-start justify-center mt-20'>
+					<div className='laptop:mr-12 order-2 laptop:order-1'>
+						<h2 className='text-5xl font-bold'>Philosophy</h2>
+						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-2xl'>
+							We identify a niche in the market for premium fitness attire and
+							outdoor equipment at economic rates. Our vision is to span this
+							void by establishing a retail framework that puts these items
+							within reach of a wider audience.
+						</p>
+						<button className='mt-5'>Read more</button>
+					</div>
+					<div className='order-1 laptop:order-2 mb-10 laptop:mb-0'>
+						<Image
+							src={'/dead-lift-man.jpg'}
+							width={370}
+							height={400}
+							alt='crunch woman'
+							className='rounded-3xl w-[476px]'
+						/>
+					</div>
 				</div>
 			</motion.div>
 		</div>
