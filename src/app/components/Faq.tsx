0fa25fe75b028b8b6c8dc915214a5d94716450ab
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-11-23 18:24:45 +0200

    feat(faq): fixed accordion width

--- src/app/components/Faq.tsx
+++ src/app/components/Faq.tsx
@@ -13,7 +13,7 @@ export default function Faq() {
 				<h2 className='text-5xl font-bold mb-14'>FAQ</h2>
 			</div>
 			<div className='flex items-center justify-center flex-col '>
-				<div className='bg-off-white px-5 py-5 w-max rounded-2xl'>
+				<div className='bg-off-white px-5 py-5 w-full max-w-max rounded-2xl'>
 					{accordionItems.map((items, index) => {
 						return (
 							<div key={index}>
