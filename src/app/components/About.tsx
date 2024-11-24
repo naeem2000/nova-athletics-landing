0a22ae7e0f71d14a3d156990a17fdb226f9e2e9f
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-11-24 19:38:18 +0200

    feat(): fixed everywhere

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -9,7 +9,7 @@ export default function About() {
 	return (
 		<div id='about' className='bg-off-white px-8 py-16 laptop:py-24'>
 			<motion.div {...motionStyles} className='max-width'>
-				<div className='grid grid-cols-1 laptop:grid-cols-2 gap-0 laptop:gap-10'>
+				<div className='grid grid-cols-1 tablet:grid-cols-2 gap-5 laptop:gap-10'>
 					<div className='mb-10 laptop:mb-0 justify-self-start laptop:justify-self-end'>
 						<Image
 							src={'/crunch-woman.jpg'}
@@ -21,7 +21,7 @@ export default function About() {
 					</div>
 					<div>
 						<h2 className='text-5xl font-bold'>Philosophy</h2>
-						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-2xl'>
+						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-xl laptop:text-2xl'>
 							{typography.about1}
 						</p>
 						<button className='mt-5'>Read more</button>
@@ -29,10 +29,10 @@ export default function About() {
 				</div>
 			</motion.div>
 			<motion.div {...motionStyles} className='max-width'>
-				<div className='grid grid-cols-1 laptop:grid-cols-2 gap-0 laptop:gap-10 mt-16'>
-					<div className='order-2 laptop:order-1 justify-self-start laptop:justify-self-end'>
+				<div className='grid grid-cols-1 tablet:grid-cols-2 gap-5 laptop:gap-10 mt-16'>
+					<div className='order-2 tablet:order-1 justify-self-start laptop:justify-self-end'>
 						<h2 className='text-5xl font-bold'>Quality & Affordability</h2>
-						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-2xl'>
+						<p className='max-w-full laptop:max-w-[476px] text-gray mt-5 text-xl laptop:text-2xl'>
 							{typography.about2}
 						</p>
 						<button className='mt-5'>Read more</button>
