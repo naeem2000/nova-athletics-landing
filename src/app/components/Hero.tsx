317cf579898dfa28cddc7f37cfd1fa300eedbb4c
Mogamat Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-12-08 13:03:14 +0200

    fix(design): design changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,23 +1,17 @@
 'use client';
 import { motion } from 'motion/react';
-import React from 'react';
+import React, { Suspense } from 'react';
 import Button from './Button';
+import VideoComponent from './VideoComponent';
 
 export default function Hero() {
 	return (
 		<motion.div className='border-t border-border-grey mt-1 laptop:p-0'>
 			<div className='relative max-width'>
-				{/* Hero Section with background video */}
 				<div className='relative w-full h-[100vh] laptop:h-[80vh] overflow-hidden'>
-					<video
-						className='absolute inset-0 w-full h-full laptop:h-max object-cover -z-10'
-						autoPlay
-						loop
-						muted
-						playsInline
-					>
-						<source src='/hero-video.mp4' type='video/mp4' />
-					</video>
+					<Suspense fallback={<p>awe</p>}>
+						<VideoComponent />
+					</Suspense>
 					<div className='absolute inset-0 bg-black h-[100vh] opacity-50 -z-5' />
 					<div className='max-width flex items-center justify-center flex-col h-full  mb-16 laptop:mb-32 z-10 relative'>
 						<h1 className='text-4xl laptop:text-8xl laptop:tracking-[-3.8px] font-bold text-center text-white'>
